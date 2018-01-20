cb1931d84c7e0c76754fe80693209e9c5075fdd0
Willian Briotto <willianbriotto@gmail.com>
2018-01-20 00:45:52 -0200

    Solve problem 'Parameters (C, sigma) for Dataset 3'

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% For both C and σ, we suggest trying values in multiplicative steps (e.g., 0.01,0.03,0.1,0.3,1,3,10,30)
+c_values = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_values = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+errors = zeros(length(c_values), length(sigma_values));
 
+for i = 1:length(c_values)
+  for j = 1: length(sigma_values)
+    model = svmTrain(X, y, c_values(i), @(x1, x2) gaussianKernel(x1, x2, sigma_values(j))); 
+    
+    % You can use the svmPredict function to generate 
+    %the predictions for the cross validation set.
+    predictions = svmPredict(model, Xval);
 
+    %  Note: You can compute the prediction error using 
+    %        mean(double(predictions ~= yval))    
+    errors(i,j) = mean(double(predictions ~= yval));
+  end
+end
 
+[ind val] = find(errors == min(min(errors)));
 
+C = c_values(ind);
+sigma = sigma_values(val);
 
 % =========================================================================
 
