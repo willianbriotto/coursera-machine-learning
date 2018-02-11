4fe4c36b0b579545ee02f614516fa5e99a6f82b1
Willian Briotto <willianbriotto@gmail.com>
2018-02-11 01:44:50 -0200

    Solve Recommender system problema ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,8 +41,11 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+                                                    %Regularization of Theta                %Regularization of Theta
+J = 1 / 2 * sum(sum(((X * Theta' - Y) .* R) .^ 2)) + ((lambda / 2) * sum(sum(Theta .^ 2))) + ((lambda / 2) * sum(sum(X .^ 2)));
 
-
+X_grad = (((X * Theta' - Y) .* R) * Theta) + (lambda * X);
+Theta_grad = ((X * Theta' - Y) .* R)' * X + (lambda * Theta);
 
 
 
